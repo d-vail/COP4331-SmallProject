312e51659c9c7a009e16a829b98389be58042780
Danny Vail <danny_vail@outlook.com>
2021-02-11 20:32:10 -0600

    REMOVE DOCKER SCRIPTS FROM FRONTEND BRANCH
    Remove Docker bash scripts. Now handled by Docker container configured in Docker Compose file.

--- restart-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ "$1" ]
-then
-  docker rm -f $1
-  bash start-docker.sh
-fi
--- start-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker build -t apache2-dev .
-docker run -dit --name local-apache2-server -p 8080:80 apache2-dev
