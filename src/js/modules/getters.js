60493149380b096c3a4d829732bc9fcf09fed919
Danny Vail <danny_vail@outlook.com>
2021-02-13 09:29:41 -0600

    DEFINE ACTION/EVENT HANDLERS
    - Map action/event handlers to API endpoints
    - Getters are responsible for retieving app data: loading app state, getting contacts, searching contacts
    - Setter are responsible for modifying app data: creating, editing, and deleting contacts

---
+++ src/js/modules/getters.js
@@ -0,0 +1,147 @@
+/**
+ * @file Event handlers responsible for retrieving app data. Includes loading the initial view of 
+ * the app, getting contacts, searching contacts.
+ */
+
+import { API_BASE, APP_BASE } from './config.js';
+import { getAuth } from './httprequests.js';
+import { getCookie, deleteCookie } from './cookies.js';
+import { renderEmptyState, renderApp, renderContactDetails } from './views.js';
+
+ /**
+  * Load the state for the application.
+  */
+ export function loadAppState(getURL = false) {
+   const token = getCookie('jwt');
+   const username = getCookie('username');
+   const url = getURL
+     ? getURL
+     : `${API_BASE}/contact/loadContacts.php?Username=${username}`;
+
+   getAuth(url, token)
+     .then(async (resp) => {
+       handleLoadResponse({
+         ok: resp.ok,
+         status: resp.status,
+         body: await resp.json(),
+       });
+     })
+     .catch((err) => {
+       console.log(err);
+       console.log(
+         'TODO: Display 500 error whole page -- base off empty state'
+       );
+     });
+ }
+
+/**
+ * Make a GET request for a single contact.
+ */
+ export function getContact() {
+   event.preventDefault();
+   const token = getCookie('jwt');
+   const username = getCookie('username');
+   const contactId = this.dataset.id;
+
+   getAuth(`${API_BASE}/contact/loadContact.php?Username=${username}&ContactID=${contactId}`, token)
+     .then(async (resp) => {
+       handleGetContactResponse({
+         ok: resp.ok,
+         status: resp.status,
+         body: await resp.json(),
+       });
+     })
+     .catch((err) => {
+       console.log(err);
+       console.log('TODO: Display 500 error contact details state -- base off empty state');
+     });
+ }
+
+/**
+ * Make a GET request for all contacts at a given page.
+ */
+export function getContacts() {
+  event.preventDefault();
+  const url = this.href;
+  const token = getCookie('jwt');
+  const username = getCookie('username');
+
+  getAuth(url, token)
+    .then(async (resp) => {
+      handleLoadResponse({
+        ok: resp.ok,
+        status: resp.status,
+        body: await resp.json(),
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log('TODO: Display 500 error whole page -- base off empty state');
+    });
+}
+
+ /**
+  * Search names of contacts using search query.
+  * @todo Show something besides blank page if no results.
+  */
+export function searchContacts() {
+  event.preventDefault();
+  const username = getCookie('username');
+  const query = encodeURI(document.querySelector('#search-contacts').value);
+  const searchURL = `${API_BASE}/contact/searchContact.php?Username=${username}&Name=${query}`;
+
+  if (query === '') {
+    loadAppState();
+  } else {
+    loadAppState(searchURL);
+  }
+}
+
+// =================
+// Internal Helpers
+// =================
+
+/**
+ * Maps actions to response from load request.
+ * @param {Object} response - Response object.
+ * @param {boolean} response.ok - A boolean indicating whether the response was successful.
+ * @param {unsigned short} response.status - The status code of the response.
+ * @param {string} response.body - The body text of the response as JSON.
+ */
+function handleLoadResponse(response) {
+  // If action is not authorized, redirect to sign in page.
+  if (response.status == 401) {
+    deleteCookie('jwt');
+    window.location.replace(APP_BASE);
+  }
+
+  // If user is authenticated but has no contacts, load empty state view. Otherwise, load the list
+  // of contacts and display the details of the first contact in the list.
+  if (response.body.count == 0)
+    renderEmptyState();
+  else
+    renderApp(response.body);
+}
+
+/**
+ * Maps actions to response from get contact request.
+ * @param {Object} response - Response object.
+ * @param {boolean} response.ok - A boolean indicating whether the response was successful.
+ * @param {unsigned short} response.status - The status code of the response.
+ * @param {string} response.body - The body text of the response as JSON.
+ */
+function handleGetContactResponse(response) {
+  // If action is not authorized, redirect to sign in page.
+  if (response.status == 401) {
+    deleteCookie('jwt');
+    window.location.replace(APP_BASE);
+  }
+
+  // If response returned with a match, display the contact details.
+  if (response.ok) {
+    renderContactDetails(response.body);
+  }
+
+  // If somehow a match could not be found, display 404 in contact details.
+  // All other errors display 500 server error in contact details.
+}
---
+++ src/js/modules/setters.js
@@ -0,0 +1,316 @@
+/**
+ * @file Event handlers responsible for modifying app data. Includes creating contacts, editing
+ * contacts, deleting contacts.
+ */
+
+import { API_BASE, APP_BASE } from './config.js';
+import { postAuth, putAuth, delAuth } from './httprequests.js';
+import { getCookie, deleteCookie } from './cookies.js';
+import { loadAppState, getContacts } from './getters.js';
+import { renderAuthorizationError, renderContactDetails } from './views.js'
+
+/**
+ * Setup add modal elements, inputs and event listeners and then display.
+ * @todo Add event listener to change image button.
+ */
+export function setupAddModal() {
+  const modalHeader = document.querySelector('#createEditContactModalLabel');
+  const modalPrimaryBtn = document.querySelector('.modal-footer .btn-primary');
+
+  // Update modal elements to indicate this action will create a new contact.
+  modalHeader.innerHTML = 'Create Contact';
+  modalPrimaryBtn.innerHTML = 'Create';
+
+  // Ensure all inputs are clear.
+  clearModalInputs();
+
+  // Reset event listeners for create action.
+  modalPrimaryBtn.removeEventListener('click', editContact);
+  modalPrimaryBtn.removeEventListener('click', createContact);
+  modalPrimaryBtn.addEventListener('click', createContact);
+}
+
+/**
+ * Setup edit modal elements, inputs and event listeners and then display.
+ * @todo Add event listener to change image button.
+ */
+export function setupEditModal() {
+  const modalHeader = document.querySelector('#createEditContactModalLabel');
+  const modalPrimaryBtn = document.querySelector('.modal-footer .btn-primary');
+
+  // Update modal elements to indicate this action will edit an existing contact.
+  modalHeader.innerHTML = 'Edit Contact';
+  modalPrimaryBtn.innerHTML = 'Save';
+
+  // Populate modal.
+  populateModalInputs();
+
+  // Reset event listeners for create action.
+  modalPrimaryBtn.removeEventListener('click', editContact);
+  modalPrimaryBtn.removeEventListener('click', createContact);
+  modalPrimaryBtn.addEventListener('click', editContact);
+}
+
+/**
+ * Make a DELETE request on a single contact.
+ */
+export function deleteContact() {
+  const token = getCookie('jwt');
+  const username = getCookie('username');
+  const contactId = this.dataset.id;
+
+  delAuth(`${API_BASE}/contact/deleteContact.php?Username=${username}&ContactID=${contactId}`, token)
+     .then(async (resp) => {
+       handleDeleteContactResponse({
+         ok: resp.ok,
+         status: resp.status,
+       });
+     })
+     .catch((err) => {
+       console.log(err);
+       console.log('TODO: Display 500 error contact details state -- base off empty state');
+     });
+}
+
+// =================
+// Internal Helpers
+// =================
+
+/**
+ * Clear all form inputs and alerts in the edit/add modal.
+ * @todo Clear image preview.
+ */
+function clearModalInputs() {
+  const modalInputs = document.querySelectorAll('#createEditContactModal input');
+  const modalSelects = document.querySelectorAll('#createEditContactModal select option');
+  const modalTextAreas = document.querySelectorAll('#createEditContactModal textarea');
+  const alerts = document.querySelectorAll('#createEditContactModal .alert');
+
+  for (let i = 0; i < modalInputs.length; i++) modalInputs[i].value = '';
+  for (let i = 0; i < modalSelects.length; i++) modalSelects[i].selected = false;
+  for (let i = 0; i < modalTextAreas.length; i++) modalTextAreas[i].value = '';
+  for (let i = 0; i < alerts.length; i++) alerts[i].parentNode.removeChild(alerts[i]);  
+}
+
+/**
+ * Populate edit/add modal with contact details.
+ * @todo Show image preview.
+ */
+function populateModalInputs() {
+  const state = getInnerText('#contact-details-state');
+
+  document.querySelector('#contact-first-name').value = getData(
+    '#contact-details-name',
+    'firstname'
+  );
+  document.querySelector('#contact-last-name').value = getData(
+    '#contact-details-name',
+    'lastname'
+  );
+  document.querySelector('#contact-email').value = getInnerText(
+    '#contact-details-email'
+  );
+  document.querySelector('#contact-phone').value = getInnerText(
+    '#contact-details-phone'
+  );
+  document.querySelector('#contact-street-address').value = getInnerText(
+    '#contact-details-street-address'
+  );
+  document.querySelector('#contact-city').value = getInnerText(
+    '#contact-details-city'
+  );
+  document.querySelector('#contact-state').value = state === null ? 'State' : state;
+  document.querySelector('#contact-zip').value = getInnerText(
+    '#contact-details-zip'
+  );
+  document.querySelector('#contact-notes').value = getInnerText(
+    '#contact-details-notes'
+  );
+}
+
+/**
+ * Close edit/add modal.
+ */
+function closeModal() {
+  bootstrap.Modal.getInstance(
+    document.querySelector('#createEditContactModal')
+  ).hide();
+}
+
+/**
+ * Make a POST request to create a new contact.
+ */
+function createContact() {
+  const token = getCookie('jwt');
+  const newContact = getFormData();
+
+  newContact.Username = getCookie('username');
+  postAuth(`${API_BASE}/contact/createContact.php`, token, newContact)
+    .then(async (resp) => {
+      handleCreateRequest({
+        ok: resp.ok,
+        status: resp.status,
+        body: await resp.json(),
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log('TODO: Display 500 error contact details state -- base off empty state');
+    });
+}
+
+/**
+ * Make a PUT request to edit an existing contact.
+ */
+function editContact() {
+  const token = getCookie('jwt');
+  const updatedContact = getFormData();
+
+  updatedContact.Username = getCookie('username');
+  updatedContact.ID = document.querySelector('.contact-details').dataset.id;
+  putAuth(`${API_BASE}/contact/updateContact.php`, token, updatedContact)
+    .then(async (resp) => {
+      handleEditRequest({
+        ok: resp.ok,
+        status: resp.status,
+        body: await resp.json(),
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log(
+        'TODO: Display 500 error contact details state -- base off empty state'
+      );
+    });
+}
+
+/**
+ * Maps actions to response from create request.
+ * @param {Object} response - Response object.
+ * @param {boolean} response.ok - A boolean indicating whether the response was successful.
+ * @param {unsigned short} response.status - The status code of the response.
+ * @param {string} response.body - The body text of the response as JSON.
+ */
+function handleCreateRequest(response) {
+  // If action is not authorized, redirect to sign in page.
+  if (response.status == 401) {
+    deleteCookie('jwt');
+    window.location.replace(APP_BASE);
+  }
+
+  const currentPage = document.querySelector('.page-item.active .page-link').innerHTML;
+
+  if (response.ok) {
+    clearModalInputs();
+    loadAppState(
+      `${API_BASE}/contact/loadContacts.php?Username=${response.body.Username}&Page=${currentPage}`
+    );
+    closeModal();
+  } else {
+    renderAuthorizationError(
+      '#createEditContactModal form',
+      response.body.Error
+    );
+  }
+}
+
+/**
+ * Maps actions to response from create request.
+ * @param {Object} response - Response object.
+ * @param {boolean} response.ok - A boolean indicating whether the response was successful.
+ * @param {unsigned short} response.status - The status code of the response.
+ * @param {string} response.body - The body text of the response as JSON.
+ * 
+ * @todo fix - update selected contact name in contact list after edit.
+ */
+function handleEditRequest(response) {
+  // If action is not authorized, redirect to sign in page.
+  if (response.status == 401) {
+    deleteCookie('jwt');
+    window.location.replace(APP_BASE);
+  }
+
+  if (response.ok) {
+    clearModalInputs();
+    renderContactDetails(response.body);
+    closeModal();
+  } else {
+    renderAuthorizationError(
+      '#createEditContactModal form',
+      response.body.Error
+    );
+  }
+}
+
+/**
+ * Maps actions to response from delete request.
+ * @param {Object} response - Response object.
+ * @param {boolean} response.ok - A boolean indicating whether the response was successful.
+ * @param {unsigned short} response.status - The status code of the response.
+ * @todo Handle additional errors returned: 404, 500
+ */
+function handleDeleteContactResponse(response) {
+  // If action is not authorized, redirect to sign in page.
+  if (response.status == 401) {
+    deleteCookie('jwt');
+    window.location.replace(APP_BASE);
+  }
+
+  // If contact was successfully deleted, reload app state.
+  if (response.status ==  204) {
+    loadAppState();
+  }
+}
+
+/**
+ * Pull contact data from create form.
+ * @returns {Object} A contact object.
+ * @todo Get image url.
+ */
+function getFormData() {
+  const state = document.querySelector('#contact-state').value;
+  const contact = {
+    FirstName: getValue('#contact-first-name'),
+    LastName: getValue('#contact-last-name'),
+    Email: getValue('#contact-email'),
+    Phone: getValue('#contact-phone'),
+    Address: getValue('#contact-street-address'),
+    City: getValue('#contact-city'),
+    State: state === 'State' ? null : state,
+    ZipCode: getValue('#contact-zip'),
+    Notes: getValue('#contact-notes'),
+  };
+
+  return contact;
+}
+
+/**
+ * Get the value of the element represented by the given CSS type selector.
+ * @param {string} selector - A CSS type selector matching target element.
+ * @returns {string|null} The value of the element or null if a value cannot be found.
+ */
+function getValue(selector) {
+  const element = document.querySelector(selector);
+  return valueExists(element) ? element.value : null;
+}
+
+/**
+ * Check is a value exists for the given Node.
+ * @param {Node} element A DOM Node
+ * @returns {boolean} A boolean indicating whether a value exists on the given Node.
+ */
+function valueExists(element) {
+  return !(element === null || element.value === undefined ||  element.value == '');
+}
+
+function getInnerText(selector) {
+  const element = document.querySelector(selector);
+  return element ? element.innerText : null;
+
+}
+
+function getData(selector, data) {
+  const element = document.querySelector(selector);
+  const dataValue = element.dataset[data];
+  return dataValue === undefined ? null : dataValue;
+}
