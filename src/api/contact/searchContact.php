43ad393d9c18549f996ee6c469cba37274a16786
Danny Vail <danny_vail@outlook.com>
2021-02-15 23:56:20 -0600

    PAGINATE THE SEARCH REULTS
    Import the same logic used to paginate loading all contacts into the search endpoint

--- src/api/contact/searchContact.php
+++ src/api/contact/searchContact.php
@@ -13,31 +13,88 @@ function searchContacts()
     $db = new DBConnection();
     $db = $db->getConnection();
 
+    // Query to Count and return total number of rows
     try
     {
-        $sql = "SELECT *
+        $sql = "SELECT COUNT(*) AS count
                 FROM Contacts
                 WHERE Username = :username AND CONCAT(FirstName, ' ',LastName) LIKE CONCAT('%', :name, '%')";
         $stmt = $db->prepare($sql);
         $stmt->bindParam(':username', $username, PDO::PARAM_STR, 255);
         $stmt->bindParam(':name', $name, PDO::PARAM_STR, 255);
         $stmt->execute();
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $rowCount = $stmt->rowCount();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    } catch (Exception $e) {
+        throw $e;
+    }
+
+    $perPage = 15;
+    $totalContacts = (int) $result['count'];
+    $totPages = ceil($totalContacts / $perPage);
+
+    if ($totPages < 1) {
+        $totPages = 1;
+    }
+
+    if (isset($_GET['Page'])) {
+        $currPage = $_GET['Page'];
+    } else {
+        $currPage = 1;
+    }
+
+    $offset = ($currPage - 1) * $perPage;
+
+    // handles case where total number of pages is only 1
+    if ($totPages == 1) {
+        $next = null;
+        $prev = null;
+    } else if ($currPage < $totPages) {
+        // if current page is 1 then prev is null
+        if ($currPage == 1) {
+            $next = "/api/contact/searchContact.php?Username=" . $username . "&Name=" . $name . "&Page=" . ($currPage + 1); // link and currPage + 1
+            $prev = null;
+        } else {
+            $next = "/api/contact/searchContact.php?Username=" . $username . "&Name=" . $name . "&Page=" . ($currPage + 1);
+            $prev = "/api/contact/searchContact.php?Username=" . $username . "&Name=" . $name . "&Page=" . ($currPage - 1);
+        }
+    } else if ($currPage == $totPages) {
+        $next = null;
+        $prev = "/api/contact/searchContact.php?Username=" . $username . "&Name=" . $name . "&Page=" . ($currPage - 1);
+    }
+
+    // Query to select limited amount of contacts to display on a page
+    try
+    {
+        $sql = "SELECT *
+                FROM Contacts
+                WHERE Username = :username AND CONCAT(FirstName, ' ',LastName) LIKE CONCAT('%', :name, '%')
+                LIMIT :perPage
+                OFFSET :offset";
+
+        $stmt = $db->prepare($sql);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+        $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $stmt->bindValue(':perPage', $perPage, PDO::PARAM_INT);
+        $stmt->execute();
+        $result2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     } catch (Exception $e) {
         Response::send(500, [
-            "Error" => "Could not fetch contact details. Please try again.",
+            "Error" => "Could not search contact details. Please try again.",
         ]);
     }
 
-    if ($rowCount == 0) {
-        Response::send(200, [
-            "results" => [],
-        ]);
-    } else {
-        Response::send(200, [
-            "results" => $result,
-        ]);
-    }
+    // set up necessary key : value pairs
+    $arr = [
+        "count" => $totalContacts,
+        "pages" => $totPages,
+        "current" => $currPage,
+        "next" => $next,
+        "prev" => $prev,
+        "results" => $result2,
+    ];
+
+    Response::send(200, $arr);
 }
